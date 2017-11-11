43c1a51a980b06c2ef6d6fe59288207fc2c037e4
Giovanni Mascellani <gio@debian.org>
2017-11-11 18:02:45 +0100

    Simple parser for where plugin.

---
+++ panda/scripts/where_reader.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2.7
+
+import sys
+import os
+import zlib
+import struct
+from google.protobuf.json_format import MessageToJson
+from os.path import dirname, join, realpath
+
+panda_dir = dirname(dirname(dirname(realpath(sys.argv[0]))))
+
+def try_path(*args):
+    args = list(args) + ['i386-softmmu']
+    build_dir = join(*args)
+    if os.path.isdir(build_dir):
+        sys.path.append(build_dir)
+try_path(panda_dir, 'build')
+try_path(panda_dir)
+try_path(dirname(panda_dir), 'opt-panda')
+try_path(dirname(panda_dir), 'debug-panda')
+try_path(dirname(panda_dir), 'panda-build')
+import plog_pb2
+
+f = open(sys.argv[1])
+
+version, _, dir_pos, _, chunk_size = struct.unpack('<IIQII', f.read(24))
+#print version, dir_pos, chunk_size
+
+f.seek(dir_pos)
+num_chunks = struct.unpack('<I', f.read(4))[0]
+#print num_chunks
+
+if num_chunks == 0:
+    sys.exit(0)
+
+entries = []
+for i in range(num_chunks):
+    buf = f.read(24)
+    entries.append(struct.unpack('<QQQ', buf))
+
+if entries[-1][1] != dir_pos:
+    entries.append((0, dir_pos, 0))
+
+#print entries
+
+import capstone
+
+class WhereParser:
+    def __init__(self):
+        self.md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
+
+    def parse_message(self, msg):
+        #print msg.where
+        #print dir(msg.where)
+        if not msg.HasField('where'):
+            return
+        where = msg.where
+        print " -- PC = %x, ASID = %x, kernel = %s" % (msg.pc, where.asid, where.in_kernel),
+        if where.HasField('filename'):
+            print ", pos = %s:0x%x%s" % (where.filename, where.pos, "" if where.unique_digest else "*")
+            with open(where.filename) as fin:
+                fin.seek(where.pos)
+                code = fin.read(where.tb_size)
+                disasm = self.md.disasm(code, where.pos)
+                for inst in disasm:
+                    print "0x%x:\t%s\t%s" %(inst.address, inst.mnemonic, inst.op_str)
+        else:
+            print ""
+
+parser = WhereParser()
+for entry, next_entry in zip(entries, entries[1:]):
+    start_instr, start_pos, num_entries = entry
+    next_pos = next_entry[1]
+    f.seek(start_pos)
+    zsize = next_pos - start_pos
+    #print start_pos, next_pos, zsize,
+    zdata = f.read(zsize)
+    data = zlib.decompress(zdata, 15, chunk_size)
+    #print len(data)
+    i = 0
+    while i < len(data):
+        entry_size = struct.unpack('<I', data[i:i+4])[0]
+        i += 4
+        entry_data = data[i:i+entry_size]
+        message = plog_pb2.LogEntry()
+        message.ParseFromString(entry_data)
+        parser.parse_message(message)
+        i += entry_size
